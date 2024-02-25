0de7aa2dce05c42d7284e86d2d9a20beb4cab614
geraldventadour <geraldventadour@users.noreply.github.com>
2024-02-25 20:51:47 +0000

    deploy: 1d661792049c8734bed00b3b7802ad327606ea04

--- js/bundle.js
+++ js/bundle.js
@@ -1,6 +1,7 @@
 
 const header = document.querySelector('header');
 const scrollWatcher = document.createElement('div');
+const scrollEndWatcher = document.querySelector('[data-scroll-end-watcher]');
 
 scrollWatcher.setAttribute('data-scroll-watcher', '');
 header.before(scrollWatcher);
@@ -9,4 +10,4 @@ const navObserver = new IntersectionObserver((entries) => {
   header.classList.toggle('sticking', !entries[0].isIntersecting);
 });
 
-navObserver.observe(scrollWatcher)
+navObserver.observe(scrollWatcher);
