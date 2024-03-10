f6ec73aa9869aa6a785ebc6c0a2babcd662f272e
geraldventadour <geraldventadour@users.noreply.github.com>
2024-03-10 19:23:26 +0000

    deploy: b92467c3b090ec4a465050d57432570726cb3428

--- js/bundle.js
+++ js/bundle.js
@@ -1,13 +1,36 @@
 
 const header = document.querySelector('header');
 const scrollWatcher = document.createElement('div');
-const scrollEndWatcher = document.querySelector('[data-scroll-end-watcher]');
+const scrollEndWatcher = document.createElement('div');
+const firstArticle = document.querySelector('article');
 
 scrollWatcher.setAttribute('data-scroll-watcher', '');
+scrollEndWatcher.setAttribute('data-scroll-end-watcher','')
 header.before(scrollWatcher);
+firstArticle.after(scrollEndWatcher);
 
 const navObserver = new IntersectionObserver((entries) => {
   header.classList.toggle('sticking', !entries[0].isIntersecting);
+ /*  header.classList.toggle('hidden', !scrollEndWatcher.isIntersecting); */
 });
 
 navObserver.observe(scrollWatcher);
+
+/* const articleObserver = new IntersectionObserver((entries) => {
+  header.classList.toggle('hidden')
+})
+
+articleObserver.observe(scrollEndWatcher); */
+const textSection = document.querySelector("section.text");
+const scrollwatcher = document.createElement('div');
+
+scrollwatcher.setAttribute('data-fade-in-up-watcher','');
+textSection.after(scrollwatcher);
+
+const fadeInObserver = new IntersectionObserver((entries) => {
+  if (entries[0].isIntersecting) {
+    textSection.classList.add('fadeInUp');
+  }
+})
+
+fadeInObserver.observe(scrollwatcher);
